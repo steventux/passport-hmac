67939ccbdd892db07321d8438435b5a844390ad8
Steve Laing <steve.laing@gmail.com>
2013-07-24 09:07:39 +0100

    WIP test default verify function

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -7,13 +7,6 @@ describe("HMAC Strategy", function() {
 
   describe("constructor", function() {
 
-    it("should fail to initialize when no verify function is supplied", function(done){
-      (function(){
-        new Strategy();
-      }).should.throw("hmac authentication strategy requires a verify function");
-      done();
-    });
-
     it("should have a name", function(done){
       new Strategy(function(){}).name.should.equal('hmac');
       done();
@@ -54,10 +47,26 @@ describe("HMAC Strategy", function() {
       strategy.authenticate("");
     });
     
+    it("should fail with no HMAC value", function(done){
+      
+      var strategy = new Strategy(function(){});
+      
+      strategy.success = function(user) {
+        this.test.error("This should never happen");
+      }
+      
+      strategy.fail = function(info) {
+        info.toString().should.equal('Error: Missing HMAC value')
+        done(null, info);
+      }
+      
+      strategy.authenticate({ body : { apiKey : '1a2b3c4d5e'} });
+    });
+    
     it("should succeed with an apiKey and a verify function", function(done){
       
-      var strategy = new Strategy(function(key, callback){
-        callback(null, { apiKey : '1a2b3c4d5e'});
+      var strategy = new Strategy(function(req, key, hmacStr, verified){
+        verified(null, {apiKey: '1a2b3c4d5e'});
       });
       
       strategy.success = function(user) {
@@ -69,7 +78,8 @@ describe("HMAC Strategy", function() {
         throw new Error("This should never happen"); 
       }
       
-      strategy.authenticate({ body: { apiKey : '1a2b3c4d5e'} });
+      strategy.authenticate(
+        { body: { apiKey : '1a2b3c4d5e', hmacValue: 'b342b3l2b352b35'}});
     });
   });
 
